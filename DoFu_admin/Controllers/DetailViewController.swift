0c892e4fd80e3628fee9a34aa52b3b2c2c02e987
Dima <ignatenkods2016@gmail.com>
2023-02-23 00:00:58 +0200

    Commit little marks to DetailViewController

--- DoFu_admin/Controllers/DetailViewController.swift
+++ DoFu_admin/Controllers/DetailViewController.swift
@@ -7,18 +7,27 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, UITextFieldDelegate {
 
     var selectedObject = ""
     var keyboardShowed = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        initializing()
+        
         print(selectedObject)
         
     }
     
+    private func initializing() {
+        // need to add hiding kb whet Return tapped - delegates for all of TF
+    }
+    
+    func textFieldShouldReturn(_ scoreText: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return true
+    }
     
     @IBAction func tapTapped(_ sender: Any) {
 
